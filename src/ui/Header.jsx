7e8c7587e366dd05256a57956a39848db7d2dc41
Tero Leppänen <tero.leppanen@live.fi>
2023-10-17 22:29:10 +0300

    Add routes and define layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>user</p>
+    </header>
+  );
+}
+
+export default Header;
