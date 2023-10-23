ec5cbb19523b5eae7117f7df5ec6dccd95f86cfe
Tero Leppänen <tero.leppanen@live.fi>
2023-10-23 15:49:42 +0300

    Setup Redux. Make username show in app

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+
 function Username() {
+  const username = useSelector((state) => state.user.username);
+
+  if (!username) return null;
+
   return (
-    <div className="hidden text-sm font-semibold md:block">Donald Duck</div>
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
   );
 }
 
